0374ebcc27df30a5f660cb24e5f57a3d650222e9
kimjunki <wemanse@naver.com>
2021-01-27 21:01:40 +0900

    0127_jwt_token_2

--- backend/src/main/java/com/example/backend/security/JwtTokenProvider.java
+++ backend/src/main/java/com/example/backend/security/JwtTokenProvider.java
@@ -15,7 +15,7 @@ public class JwtTokenProvider {
     private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
 
     // @Value("${app.jwtSecret}")
-    private String jwtSecret;
+    private String jwtSecret; 
 
     // @Value("${app.jwtExpirationInMs}")
     private int jwtExpirationInMs;
