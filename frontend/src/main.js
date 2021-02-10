857e6b15e246904bd436738ceb0af291055f67f0
dasom <ektha3869@naver.com>
2021-02-10 19:40:06 +0900

    0210_likenews_frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueCookies from 'vue-cookies'
-
+import JwPagination from 'jw-vue-pagination'
 
 Vue.config.productionTip = false;
-
 Vue.use(VueCookies)
+Vue.component('jw-pagination', JwPagination)
 
 new Vue({
   router,
